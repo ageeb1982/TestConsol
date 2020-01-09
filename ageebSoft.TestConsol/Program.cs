7013d45c7ba57ac62863dea65462dbbe9b555885
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2020-01-09 18:38:35 +0300

    Add project files.

---
+++ ageebSoft.TestConsol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ageebSoft.TestConsol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
